383a851e6742a37c23cc94c282bfa640875b9763
Aleksey Morev <morevbalex777@gmail.com>
2021-01-14 16:30:55 +0300

    finish

---
+++ weather/src/components/Container.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="informer-container">
+        <Temperature v-bind:real="weather.temp_real" v-bind:feel="weather.temp_feel" />
+        <div class="flex-break"></div>
+        <Humidity v-bind:val="weather.humidity" />
+        <div class="vertical-divider"></div>
+        <Wind v-bind:val="weather.wind" />
+    </div>
+</template>
+
+<script>
+import Humidity from './Humidity.vue'
+import Temperature from './Temperature.vue'
+import Wind from './Wind.vue'
+
+export default {
+    name: "Container",
+    props: ['weather'],
+    components: { 
+        Temperature,
+        Wind,
+        Humidity
+    },
+    
+}
+</script>
+
+<style>
+.informer-container {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 400px;
+  padding: 30px;
+  background-image: url('/img/forest.png');
+  background-size: cover;
+  background-position: center center;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.75);
+}
+.flex-break{
+  flex-basis: 100%;
+  height: 0;
+}
+
+.vertical-divider{
+  height: 70px;
+  width: 1px;
+  background-color: #fff;
+  vertical-align: middle;
+  display: table-cell;
+}
+</style>
